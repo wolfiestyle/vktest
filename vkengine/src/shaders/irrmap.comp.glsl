6094e6d5d9534340dab8eed4aaa3c0691f05ba5c
SnowDerg <slayerbeast@gmail.com>
2023-04-19 16:38:29 -0400

    make var name consistent

--- vkengine/src/shaders/irrmap.comp.glsl
+++ vkengine/src/shaders/irrmap.comp.glsl
@@ -15,8 +15,8 @@ void main() {
     vec3 color = vec3(0.0);
     float total_weight = 0.0;
     for (uint i = 0; i < NumSamples; ++i) {
-        vec2 u = sampleHammersley(i, NumSamples);
-        vec3 tangent_dir = sampleHemisphere(u);
+        vec2 Xi = sampleHammersley(i, NumSamples);
+        vec3 tangent_dir = sampleHemisphere(Xi);
         vec3 world_dir = S * tangent_dir.x + T * tangent_dir.y + N * tangent_dir.z;
         float cosTheta = max(dot(world_dir, N), 0.0);
         color += texture(inputTex, world_dir).rgb * cosTheta;
